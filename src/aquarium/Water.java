1865a1c61d42c158f7847e1fabfe53276f63dac1
Mauri Segura <theasker@gmail.com>
2017-03-11 13:59:51 +0100

    exemen carpeta 1

---
+++ src/aquarium/Water.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aquarium;
+
+/**
+ *
+ * @author theasker
+ */
+public class Water {
+    
+}
---
+++ src/structure/Varios.java
@@ -0,0 +1,11 @@
+package structure;
+
+import java.util.*;
+
+public class Varios {
+    String name;
+    int entero = 4.3;
+    public static void main(String[] args) {
+        
+    }
+}

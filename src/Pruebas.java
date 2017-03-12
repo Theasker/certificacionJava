3fa884f1e1ebb94c1ea300d965ac7f10cc6693d4
Mauricio Segura (Theasker) <theasker@gmail.com>
2017-03-12 19:00:00 +0100

    capitulo 1, calificación

---
+++ src/Pruebas.java
@@ -0,0 +1,14 @@
+
+public class Pruebas {
+
+    int count;
+
+    public void Pruebas() {
+        count = 4;
+    }
+
+    public static void main(String[] args) {
+        Pruebas s = new Pruebas();
+        System.out.println(s.count);
+    }
+}
